b332a235a9d943a9ffbb33d64c5834dc72806005
Aayurt <aayurtshrestha@gmail.com>
2023-05-03 19:19:59 +0100

    Changes

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys');
 
 // Connect to DB
 mongoose
-  .connect(db, {
+  .connect(db.mongoURI, {
     dbName: db.dbName,
   })
   .then(() => console.log('Connected to database'))
