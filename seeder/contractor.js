d310860acf80cee1b0ed8b3b8aa9eea9094cf76d
Gurung-Manish <manishgrg496@gmail.com>
2023-09-01 19:46:00 +0100

    new seeders for customer and contractor

--- seeder/contractor.js
+++ seeder/contractor.js
@@ -1,5 +1,5 @@
-const { faker } = require('@faker-js/faker');
-const bcrypt = require('bcryptjs');
+const { faker } = require("@faker-js/faker");
+const bcrypt = require("bcryptjs");
 
 const generateRandomLocation = () => {
   // Define longitude and latitude ranges for London
@@ -19,7 +19,7 @@ const generateRandomLocation = () => {
   });
 
   return {
-    type: 'Point',
+    type: "Point",
     coordinates: [latitude, longitude],
   };
 };
@@ -42,9 +42,9 @@ const generateRandomEducation = () => {
 };
 
 const generateRandomProfile = async () => {
-  let password = '';
+  let password = "";
   await bcrypt
-    .hash('111111', 10)
+    .hash("111111", 10)
     .then((hash) => {
       password = hash;
     })
@@ -53,10 +53,10 @@ const generateRandomProfile = async () => {
     name: faker.person.fullName(),
     email: faker.internet.email(),
     password: password,
-    gender: faker.helpers.arrayElement(['male', 'female', 'others']),
+    gender: faker.helpers.arrayElement(["male", "female", "others"]),
     phone: faker.phone.number(),
     bio: faker.lorem.sentence(),
-    avatar: '/uploads/profile/test.jpg',
+    avatar: "/uploads/profile/test.jpg",
     address: faker.location.streetAddress(),
     location: generateRandomLocation(),
     linkedIn: faker.internet.url(),
@@ -73,70 +73,247 @@ const generateRandomProfile = async () => {
 
 const numberOfProfiles = 10;
 const contractorData = [];
+let password = "$2a$10$BgTgNprlqyyM1D4Fsp7A5u9tyFoaAt95dWCl9AWpGcwwoy0Zz.QYq";
+// await bcrypt
+//   .hash("111111", 10)
+//   .then((hash) => {
+//     password = hash;
+//   })
+//   .catch((err) => console.error(err.message));
 
-for (let i = 0; i < numberOfProfiles; i++) {
-  contractorData.push({
-    name: 'Manish',
-    email: 'mg@gmail.com',
-    password: password,
-    phone: '1-418-653-8049',
-    bio: 'Facilis saepe blanditiis non magni vel qui ipsum numquam error.',
-    gender: 'female',
-    avatar: '/uploads/profile/test.jpg',
-    address: '5769 Cremin Divide',
-    location: { type: 'Point', coordinates: [51.514852, 0.127262] },
-    skills: ['reflect', 'tailgate', 'avow'],
-    experiences: [
-      {
-        employerName: 'Ziemann - Walter',
-        startYear: '2022',
-        role: 'Dynamic Infrastructure Planner',
-        endYear: '2023',
-        _id: { $oid: '64e77720747aa46ca6c44f5e' },
-      },
-    ],
-    education: [
-      {
-        instituteName: 'Lowe LLC',
-        startYear: '2023',
-        endYear: '2023',
-        _id: { $oid: '64e77720747aa46ca6c44f5f' },
-      },
-    ],
-    numRatings: 0,
-    ratingSum: 0,
-  });
-  contractorData.push({
-    name: 'ManishG',
-    email: 'mg1@gmail.com',
-    password: password,
-    phone: '1-418-653-8049',
-    bio: 'Facilis saepe blanditiis non magni vel qui ipsum numquam error.',
-    gender: 'female',
-    avatar: '/uploads/profile/test.jpg',
-    address: '5769 Cremin Divide',
-    location: { type: 'Point', coordinates: [51.514852, 0.127262] },
-    skills: ['reflect', 'tailgate', 'avow'],
-    experiences: [
-      {
-        employerName: 'Ziemann - Walter',
-        startYear: '2022',
-        role: 'Dynamic Infrastructure Planner',
-        endYear: '2023',
-        _id: { $oid: '64e77720747aa46ca6c44f5e' },
-      },
-    ],
-    education: [
-      {
-        instituteName: 'Lowe LLC',
-        startYear: '2023',
-        endYear: '2023',
-        _id: { $oid: '64e77720747aa46ca6c44f5f' },
-      },
-    ],
-    numRatings: 0,
-    ratingSum: 0,
-  });
-}
+contractorData.push({
+  name: "Manish Gurung",
+  email: "mg@gmail.com",
+  password: password,
+  phone: "07470259087",
+  bio: "I am an electrician that can take care of all your household electric needs.",
+  gender: "male",
+  avatar: "/uploads/profile/test.jpg",
+  address: "9 Friday Road, DA8 1PL",
+  location: { type: "Point", coordinates: [51.4839, 0.1666] },
+  skills: ["wiring", "welding"],
+  experiences: [
+    {
+      employerName: "Aayurt Shrestha",
+      startYear: "2021",
+      role: "Electric Technicial",
+      endYear: "2022",
+    },
+  ],
+  education: [
+    {
+      instituteName: "Kingston University",
+      startYear: "2020",
+      endYear: "2021",
+    },
+  ],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+contractorData.push({
+  name: "Emily Turner",
+  email: "emilyt@gmail.com",
+  password: password,
+  phone: "07551234567",
+  bio: "Experienced painter offering high-quality painting services for your home or business.",
+  gender: "female",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "59 Eardley Rd, Belvedere DA17 6EX",
+  location: { type: "Point", coordinates: [51.485146, -0.150388] },
+  skills: ["painting", "color mixing"],
+  experiences: [
+    {
+      employerName: "BrightBrush Painters",
+      startYear: "2020",
+      role: "Painter",
+      endYear: "2022",
+    },
+  ],
+  education: [],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+contractorData.push({
+  name: "Michael Brown",
+  email: "michaelb@gmail.com",
+  password: password,
+  phone: "07481234567",
+  bio: "Skilled electrician providing expert electrical services for residential and commercial clients.",
+  gender: "male",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "7 Birkdale Rd, London SE2 9HU",
+  location: { type: "Point", coordinates: [51.48958, 0.103737] },
+  skills: ["wiring", "circuit design"],
+  experiences: [
+    {
+      employerName: "EcoSpark Electric",
+      startYear: "2018",
+      role: "Electrician",
+      endYear: "2023",
+    },
+  ],
+  education: [
+    {
+      instituteName: "London Electrical Academy",
+      startYear: "2017",
+      endYear: "2018",
+    },
+  ],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+contractorData.push({
+  name: "Sophie Martinez",
+  email: "sophiem@gmail.com",
+  password: password,
+  phone: "07460234567",
+  bio: "Experienced hairdresser specializing in haircuts, styling, and color treatments.",
+  gender: "female",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "22 Vicarage Rd, London SE18 7SP",
+  location: { type: "Point", coordinates: [51.484862, 0.077334] },
+  skills: ["haircutting", "styling"],
+  experiences: [
+    {
+      employerName: "Glamour Cuts Salon",
+      startYear: "2019",
+      role: "Hairdresser",
+      endYear: "2023",
+    },
+  ],
+  education: [],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+contractorData.push({
+  name: "Daniel Lopez",
+  email: "daniell@gmail.com",
+  password: password,
+  phone: "07470234567",
+  bio: "Dedicated caregiver providing compassionate support and assistance to individuals in need.",
+  gender: "male",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "Central Court, 111 Woolwich New Rd, London SE18 6EZ",
+  location: { type: "Point", coordinates: [51.4879, 0.0663] },
+  skills: ["caregiving", "companionship"],
+  experiences: [
+    {
+      employerName: "ElderlyCare Services",
+      startYear: "2020",
+      role: "Caregiver",
+      endYear: "2023",
+    },
+  ],
+  education: [
+    {
+      instituteName: "London Care Institute",
+      startYear: "2019",
+      endYear: "2020",
+    },
+  ],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+contractorData.push({
+  name: "David Mitchell",
+  email: "davidm@gmail.com",
+  password: password,
+  phone: "07480234567",
+  bio: "Experienced plumber specializing in pipe repairs, installations, and maintenance.",
+  gender: "male",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "17 Mc Call Cres, London SE7 8HS",
+  location: { type: "Point", coordinates: [51.484238, 0.045345] },
+  skills: ["plumbing", "pipe fitting"],
+  experiences: [
+    {
+      employerName: "SwiftFlow Plumbing",
+      startYear: "2018",
+      role: "Plumber",
+      endYear: "2023",
+    },
+  ],
+  education: [],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+contractorData.push({
+  name: "Laura Turner",
+  email: "laurat@gmail.com",
+  password: password,
+  phone: "07451234567",
+  bio: "Skilled carpenter offering custom woodwork, furniture construction, and repairs.",
+  gender: "female",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "9a Ripley Rd, London E16 3EA",
+  location: { type: "Point", coordinates: [51.513498, 0.033064] },
+  skills: ["carpentry", "woodworking"],
+  experiences: [
+    {
+      employerName: "CraftWood Creations",
+      startYear: "2019",
+      role: "Carpenter",
+      endYear: "2023",
+    },
+  ],
+  education: [],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+// Gardeners
+contractorData.push({
+  name: "Robert Green",
+  email: "robertg@gmail.com",
+  password: password,
+  phone: "07461234567",
+  bio: "Professional gardener offering landscaping, lawn care, and garden maintenance services.",
+  gender: "male",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "12A Hartley Ave, London E6 1NT",
+  location: { type: "Point", coordinates: [51.535, 0.0545] },
+  skills: ["gardening", "landscaping"],
+  experiences: [
+    {
+      employerName: "GreenThumb Gardens",
+      startYear: "2020",
+      role: "Gardener",
+      endYear: "2023",
+    },
+  ],
+  education: [],
+  numRatings: 0,
+  ratingSum: 0,
+});
+
+contractorData.push({
+  name: "Alice Wood",
+  email: "alicew@gmail.com",
+  password: password,
+  phone: "07472234567",
+  bio: "Passionate gardener with a green thumb, specializing in plant care and garden design.",
+  gender: "female",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "189 Longlands Rd, Sidcup DA15 7LB",
+  location: { type: "Point", coordinates: [51.432733, 0.099412] },
+  skills: ["gardening", "plant care"],
+  experiences: [
+    {
+      employerName: "Blooming Gardens",
+      startYear: "2019",
+      role: "Gardener",
+      endYear: "2023",
+    },
+  ],
+  education: [],
+  numRatings: 0,
+  ratingSum: 0,
+});
 
 module.exports = { contractorData };
--- seeder/customer.js
+++ seeder/customer.js
@@ -1,5 +1,5 @@
-const { faker } = require('@faker-js/faker');
-const bcrypt = require('bcryptjs');
+const { faker } = require("@faker-js/faker");
+const bcrypt = require("bcryptjs");
 
 const generateRandomLocation = () => {
   // Define longitude and latitude ranges for London
@@ -19,15 +19,15 @@ const generateRandomLocation = () => {
   });
 
   return {
-    type: 'Point',
+    type: "Point",
     coordinates: [latitude, longitude],
   };
 };
 
 const generateRandomCustomer = async () => {
-  let password = '';
+  let password = "";
   await bcrypt
-    .hash('111111', 10)
+    .hash("111111", 10)
     .then((hash) => {
       password = hash;
     })
@@ -38,7 +38,7 @@ const generateRandomCustomer = async () => {
     password: password,
     phone: faker.phone.number(),
     bio: faker.lorem.sentence(),
-    avatar: '/uploads/profile/test.jpg',
+    avatar: "/uploads/profile/test.jpg",
     address: faker.location.streetAddress(),
     location: generateRandomLocation(),
   };
@@ -46,18 +46,102 @@ const generateRandomCustomer = async () => {
 
 const numberOfCustomers = 10;
 const customerData = [];
+let password = "$2a$10$BgTgNprlqyyM1D4Fsp7A5u9tyFoaAt95dWCl9AWpGcwwoy0Zz.QYq";
+// await bcrypt
+//   .hash("111111", 10)
+//   .then((hash) => {
+//     password = hash;
+//   })
+//   .catch((err) => console.error(err.message));
 
-for (let i = 0; i < numberOfCustomers; i++) {
-  customerData.push({
-    name: 'test',
-    email: 'test2@gmail.com',
-    password: '$2a$10$7r3oXPP4opNLVUBeGdrrEeveep720I1kTVBQxXLEoUtH4w9SN.1yS',
-    gender: 'male',
-    phone: '(533) 518-7340 x280',
-    bio: 'Omnis quas inventore ipsam tenetur aliquam.',
-    avatar: '/uploads/profile/test.jpg',
-    address: '26 Strand Court, 33 Strandfield Close',
-    location: { type: 'Point', coordinates: [51.5, 0.127] },
-  });
-}
+customerData.push({
+  name: "test",
+  email: "test2@gmail.com",
+  password: password,
+  gender: "male",
+  phone: "07450829056",
+  bio: "Hello, I am a 21 year old male looking for home improvements.",
+  avatar: "/uploads/profile/test.jpg",
+  address: "26 Strand Court, 33 Strandfield Close",
+  location: { type: "Point", coordinates: [51.5, 0.127] },
+});
+
+// Customer 1
+customerData.push({
+  name: "John Smith",
+  email: "john.smith@gmail.com",
+  password: password,
+  gender: "male",
+  phone: "07460234567",
+  bio: "Hi, I'm John, a homeowner interested in home renovation projects.",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "4 Nairne Grove, London SE24 9PU",
+  location: { type: "Point", coordinates: [51.4576, -0.0904] },
+});
+
+// Customer 2
+customerData.push({
+  name: "Emily Johnson",
+  email: "emily.j@gmail.com",
+  password: password,
+  gender: "female",
+  phone: "07471234567",
+  bio: "Hello, I'm Emily, and I'm looking to enhance my living space with interior design.",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "3 Brantwood Rd, London SE24 0DH",
+  location: { type: "Point", coordinates: [51.4581, -0.0992] },
+});
+
+// Customer 3
+customerData.push({
+  name: "Sophie Martinez",
+  email: "sophie.m@gmail.com",
+  password: password,
+  gender: "female",
+  phone: "07482234567",
+  bio: "Hey there, I'm Sophie, and I'm excited to make my garden beautiful.",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "41 Heath Rd, Thornton Heath CR7 8NF",
+  location: { type: "Point", coordinates: [51.4031, -0.0994] },
+});
+
+// Customer 4
+customerData.push({
+  name: "Daniel Lopez",
+  email: "daniel.l@gmail.com",
+  password: password,
+  gender: "male",
+  phone: "07493234567",
+  bio: "Hi, I'm Daniel, and I need some help with home repairs and maintenance.",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "65 Furze Rd, Thornton Heath CR7 8NJ",
+  location: { type: "Point", coordinates: [51.4039, -0.0978] },
+});
+
+// Customer 5
+customerData.push({
+  name: "Laura Turner",
+  email: "laura.t@gmail.com",
+  password: password,
+  gender: "female",
+  phone: "07460234568", // Different phone number
+  bio: "Hello, I'm Laura, and I'm interested in home improvement and renovations.",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "7 Greenleaf Rd, London E6 1DX",
+  location: { type: "Point", coordinates: [51.5348, 0.0367] },
+});
+
+// Customer 6
+customerData.push({
+  name: "David Mitchell",
+  email: "david.m@gmail.com",
+  password: password,
+  gender: "male",
+  phone: "07470234568", // Different phone number
+  bio: "Hi, I'm David, and I'm looking to remodel my kitchen and bathroom.",
+  avatar: "/uploads/profile/test.jpg", // Use the same avatar path
+  address: "107 Harold Rd, London E13 0SG",
+  location: { type: "Point", coordinates: [51.5348, 0.0316] },
+});
+
 module.exports = { customerData };
