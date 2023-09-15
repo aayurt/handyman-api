f270beed5435184afa111543d2273e016473bfd9
Aayurt <aayurtshrestha@gmail.com>
2023-09-15 18:32:46 +0100

    Changes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const compression = require('compression');
 var cors = require('cors');
 const helmet = require('helmet');
 const path = require('path');
+const os = require('os');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -39,6 +40,24 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/pdf', require('./routes/api/pdf'));
 app.use('/api/file', require('./routes/api/image'));
 
+const getLocalIpAddress = () => {
+  const ifaces = os.networkInterfaces();
+  let ipAddress = '';
+
+  Object.keys(ifaces).forEach((ifname) => {
+    ifaces[ifname].forEach((iface) => {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        ipAddress = iface.address;
+      }
+    });
+  });
+
+  return ipAddress;
+};
+
 app.listen(PORT, () => {
+  const ipAddress = getLocalIpAddress();
+  console.log(`Server is running on ${ipAddress}:${PORT}`);
+
   console.log(`Server is running on port ${PORT}`);
 });
